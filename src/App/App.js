66a31d43a13f72e3b5a9ef197168b51778221fa3
adengz <adengz@gmail.com>
2020-11-28 00:50:13 +0800

    feat: Add sketch app ui

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,34 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { fetchInitialData } from '../actions';
+import PersonList from '../components/PersonList';
 
 const App = () => {
+  const { trainers, trainees } = useSelector(({ ungrouped }) => ungrouped);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchInitialData());
   }, []);
 
-  return <div>Hello World</div>;
+  return (
+    <>
+      <section>
+        <header>
+          <h2>分组列表</h2>
+          <button type="submit">分组学员</button>
+        </header>
+      </section>
+      <section>
+        <h2>讲师列表</h2>
+        <PersonList data={trainers} />
+      </section>
+      <section>
+        <h2>学员列表</h2>
+        <PersonList data={trainees} />
+      </section>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/PersonList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PersonList = ({ data }) => {
+  return <div>{JSON.stringify(data)}</div>;
+};
+
+export default PersonList;
