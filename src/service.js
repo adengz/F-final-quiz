a2b63e1a6e07d9149b3ad6f04da0c87d948b6242
adengz <adengz@gmail.com>
2020-11-28 05:16:39 +0800

    feat: Align service with backend api

--- src/service.js
+++ src/service.js
@@ -1,17 +1,31 @@
 const HOST = 'http://localhost:8080';
 const headers = { 'Content-Type': 'application/json' };
 
-export const fetchTrainers = async () => {
-  const response = await fetch(`${HOST}/trainers`, { headers });
+export const getUngrouped = async (title) => {
+  const response = await fetch(`${HOST}/${title}?grouped=false`, { method: 'GET', headers });
   return response.ok ? response.json() : Promise.reject(response);
 };
 
-export const fetchTrainees = async () => {
-  const response = await fetch(`${HOST}/trainees`, { headers });
+export const getGroups = async () => {
+  const response = await fetch(`${HOST}/groups`, { method: 'GET', headers });
   return response.ok ? response.json() : Promise.reject(response);
 };
 
-export const fetchGroups = async () => {
-  const response = await fetch(`${HOST}/groups`, { headers });
+export const postPerson = async (title, name) => {
+  const response = await fetch(`${HOST}/${title}`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({ name }),
+  });
+  return response.ok ? response.json() : Promise.reject(response);
+};
+
+export const deletePerson = async (title, id) => {
+  const response = await fetch(`${HOST}/${title}/${id}`, { method: 'DELETE', headers });
+  return response.ok ? response.json() : Promise.reject(response);
+};
+
+export const postGroups = async () => {
+  const response = await fetch(`${HOST}/groups/auto-grouping`, { method: 'POST', headers });
   return response.ok ? response.json() : Promise.reject(response);
 };
